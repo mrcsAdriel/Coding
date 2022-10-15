441655eccaf9881593a8592417746db395f86884
mrcsAdriel <114810763+mrcsAdriel@users.noreply.github.com>
2022-10-15 12:01:54 -0300

    Add files via upload

---
+++ codigo_html_coding-main/pratica/index.js
@@ -0,0 +1,40 @@
+/*let x = 1; Let é pra abrir uma variavel 
+const y = 1; const é uma variavel constante que não muda seu elemento 
+console
+|| = ou
+&& = e*/
+
+if (1 < 2) {
+
+}else if ( 2 == 2 && 5 == 5){
+
+} else {
+
+}
+
+while(true) {
+    
+}
+
+for (let index = 0; index < lista.length; index++) {
+    console.log(lista[index])
+
+}
+
+lista.forEach((num)=>{
+    console.log(num)
+})
+
+function (params) {
+    
+}
+
+let lista = [1,2,3,4,5]
+
+let function = () => {
+
+    
+}
+    
+}
+
